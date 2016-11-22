f70d09e7ebeb1ddc0b3c3fc9ab4a40bac7e9bb0b
Luca Cattide <luca@laboratorio-a.it>
2016-11-22 16:25:00 +0100

    FMA
    Full-Stack Setup:
    
    - NodeJS
    - Gulp
    - Webpack
    - Firebase (debug)

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebase = require("firebase");
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCLFPY6AcZFzqrDUFp7rgpAb3e4-EIgKtw",
+	authDomain: "fitmeapp-442dd.firebaseapp.com",
+	databaseURL: "https://fitmeapp-442dd.firebaseio.com",
+	storageBucket: "fitmeapp-442dd.appspot.com",
+	messagingSenderId: "118454996427"
+};
+firebase.initializeApp(config);
