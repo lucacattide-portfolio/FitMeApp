97a6b9b1f4f6a946b88d3cbf30a84d3dd5b6badc
Matteo Sasso <matteo@laboratorio-a.it>
2016-11-22 18:46:08 +0100

    Diverse modifiche: - Committato yarn.lock come da best practice - Sorgenti Javascript in src/ - Aggiunti babel e clang-format al gulpfile - Aggiunti lib/ e *.log a .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,38 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const del = require('del');
+const exec = require('child_process').exec;
+const format = require('gulp-clang-format');
 
-gulp.task('default', function() {
-  // place code for your default task here
+const paths = {
+  allSrcJs: 'src/**/*.js',
+  libDir: 'lib',
+};
+
+gulp.task('clean', () => {
+  return del(paths.libDir);
 });
+
+gulp.task('check-format', function() {
+  return gulp.src(paths.allSrcJs)
+     .pipe(format.checkFormat());
+});
+
+gulp.task('build', ['clean', 'check-format'], () => {
+  return gulp.src(paths.allSrcJs)
+    .pipe(babel())
+    .pipe(gulp.dest(paths.libDir));
+});
+
+gulp.task('main', ['build'], (callback) => {
+  exec(`node ${paths.libDir}`, (error, stdout) => {
+    console.log(stdout);
+    return callback(error);
+  });
+});
+
+gulp.task('watch', () => {
+  gulp.watch(paths.allSrcJs, ['main']);
+});
+
+gulp.task('default', ['watch', 'main']);
