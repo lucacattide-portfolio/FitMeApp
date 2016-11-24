f232ed912f7f3c3c88db89be949313e32fc460f2
Luca Cattide <luca@laboratorio-a.it>
2016-11-24 10:05:46 +0100

    Pulizia Repository
    Rimozione junk files:
    
    - Chiavi SSH private;
    - LEGGIMI.txt;
    - Vecchie configurazioni di prova Firebase;

--- admin-firebase.js
+++
@@ -1,6 +0,0 @@
-var admin = require("firebase-admin");
-
-admin.initializeApp({
-  credential: admin.credential.cert("fitmeapp-442dd-firebase-adminsdk-y0az4-158cc4b210.json"),
-  databaseURL: "https://fitmeapp-442dd.firebaseio.com"
-});
