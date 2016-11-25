1bc2a95dae302edd29fd956d02a495c222bcbcdb
Luca Cattide <luca@laboratorio-a.it>
2016-11-25 15:31:21 +0100

    Sviluppo
    - Inclusion moduli jQuery/Firebase;
    - Implementazione Layout testing;
    
    Bugs:
    
    - I moduli jQuery non partono;
    - Vari ed eventuali (leggere i commenti);

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-// Importazione modulo Firebase
-let firebase = require('firebase');
+// Definizione path inclusioni
+
+// TODO: Path statica per attingere direttamente da node_modules
+// per evitando duplicati - non funziona. Forse sovrascritta, Verificare.
+
+// app.use('jquery-mobile',
+// express.static(__dirname + '/node-modules/jquery-mobile/dist/'));
+
+// Importazione moduli
+let $ = require('./jquery');
+$.mobile = require('./jquery-mobile');
+let firebase = require('./firebase');
+
+// TODO: ESlint da errore con questa direttiva. Verificare
+// import { firebase } from './firebase';
+
 // Inizializzazione Firebase
-let config = {
+firebase.config = {
   apiKey: 'AIzaSyAssBBEv9KKOeqH0lFVDemuC5FE21-i8lo',
   authDomain: 'fitmeapp-64482.firebaseapp.com',
   databaseURL: 'https://fitmeapp-64482.firebaseio.com',
